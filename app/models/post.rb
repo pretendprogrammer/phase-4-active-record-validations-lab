95a436943cbd82f97f3e2b1e9c51b3a4cc5312f1
Yosef <shmuli5770@gmail.com>
2021-06-09 16:35:57 -0400

    commit tree c74a7d99e8e2680d41735599730d9547aa344bd9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :must_not_be_clickbait
+
+    def must_not_be_clickbait
+        unless title != "True Facts"
+          errors.add(:title, "That's BS.")
+        end
+    end
 end
